4e8f2d3e3a1f0463d5f17b257f128b9e6fe9f6fc
Kenneth Brezinski <kenbrezinski@live.com>
2023-02-06 12:27:48 -0600

    added multi attention head mechanism

--- definitions/models.py
+++ definitions/models.py
@@ -3,8 +3,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+# Single attention head implementaiton
 class AttentionHead(nn.Module):
-    def __init__(self, embed_dim, block_size=8, head_size=32):
+    def __init__(self, embed_dim, block_size, head_size):
         super().__init__()
         self.q = torch.nn.Linear(embed_dim, head_size, bias=False)
         self.k = torch.nn.Linear(embed_dim, head_size, bias=False)
@@ -12,29 +14,54 @@ class AttentionHead(nn.Module):
         self.register_buffer('tril_mask',
                  torch.tril(torch.ones(block_size, block_size)))
 
-
     def forward(self, tokens):
         batch, block, embed_dim = tokens.shape
         # init token projection
-        k = self.k(tokens)
+        k = self.k(tokens) 
         q = self.q(tokens)
         # query key attention; normalize by sqrt(d_k)
-        wei = torch.matmul(q, k.view(batch, embed_dim, block)).pow(-0.5)
-        wei = wei.masked_fill(self.tril_mask == 0, -1e9).softmax(dim=-1)
+        wei = q @ k.transpose(-2, -1) * (embed_dim ** -0.5)
+        wei = wei.masked_fill(self.tril_mask[:block, :block] == 0, -1e9).softmax(dim=-1)
         # weighted aggregation
         v = self.v(tokens)
         output = torch.matmul(wei, v)
         return output
 
 
+# Multihead attention implementation
+class MultiAttention(nn.Module):
+    def __init__(self, num_heads, block_size, embed_dim):
+        super().__init__()
+        head_size = embed_dim // num_heads
+        self.heads = nn.ModuleList(
+            [AttentionHead(embed_dim, block_size, head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
+# Transformer block implementation
+class TransformerBlock(nn.Module):
+    def __init__(self, num_heads, block_size, embed_dim):
+        super().__init__()
+        self.attention = MultiAttention(num_heads, block_size, embed_dim)
+        self.linear = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, x):
+        x = self.attention(x)
+        x = self.linear(x).relu()
+        return x
+
+
 class BiGram(nn.Module):
     
-    def __init__(self, vocab_size, embed_dim=32):
+    def __init__(self, vocab_size, embed_dim, block_size, num_heads):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embed_dim)
         self.pos_embedding = nn.Embedding(vocab_size, embed_dim)
-        self.attn_head = AttentionHead(embed_dim)
-        self.linear_head = nn.Linear(embed_dim, vocab_size)
+        self.attn_block = nn.Sequential(
+            *[TransformerBlock(num_heads, block_size, embed_dim) for _ in range(3)])
+        self.linear = nn.Linear(embed_dim, vocab_size)
 
     def forward(self, tokens, target=None):
         batch, block_size = tokens.shape
@@ -45,8 +72,8 @@ class BiGram(nn.Module):
         pos_embed = self.pos_embedding(torch.arange(block_size))
         x = token_embed + pos_embed
 
-        x = self.attn_head(x)
-        logits = self.linear_head(x)
+        x = self.attn_block(x)
+        logits = self.linear(x)
         
         # (batch*block, vocab_size)
         if target is None:
@@ -66,9 +93,10 @@ class BiGram(nn.Module):
             idx = torch.zeros((1, 1), dtype=torch.long)
 
         for _ in range(max_new_tokens):
+            # crop the block size so that pos embedding is within scope
             idx_cropped = idx[:, -block_size:]
             # (batch, block, vocab_size)
-            logits, _ = self.forward(idx)
+            logits, _ = self.forward(idx_cropped)
             # (1, seq_len, vocab_size)
             logits = logits[:, -1, :]
             # (1, 1, vocab_size)
--- utils/constants.py
+++ utils/constants.py
@@ -4,10 +4,13 @@ seed = 42
 
 # dataset
 block_size = 8
+embed_dim = 32
+num_heads = 4
+
 batch_size = 32
 train_frac = 0.9
 
 # training
-num_epochs = 3_000
+num_epochs = 5_000
 eval_iters = 250
 lr = 1e-2
